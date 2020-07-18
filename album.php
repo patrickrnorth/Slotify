7a39f1f7b965ab2f85ca714b6dba5c1c1e452b1a
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-07-18 18:25:39 -0400

    add update to mainContainer

---
+++ album.php
@@ -0,0 +1,25 @@
+<?php include("includes/header.php");
+if(isset($_GET['id'])) {
+    $albumId = $_GET['id'];
+}
+else {
+    header("Location: index.php");
+}
+
+$album = new Album($con, $albumId);
+
+$artist = $album->getArtist();
+
+echo $album->getTitle() . "<br>";
+echo $artist->getName();
+
+?>
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php");?>

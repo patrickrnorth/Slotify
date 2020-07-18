2123cc083b7b84adedce7c4660d84c0011d8037f
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-07-18 15:47:33 -0400

    add split to index page and worked on mainContent
    also add assets for db and db

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<?php
+include("includes/config.php");
+
+//session_destroy(); LOGOUT TEMPORARILY
+
+if(isset($_SESSION['userLoggedIn']))
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+else {
+	header("Location: register.php");
+}
+?>
+
+<html>
+<head>
+	<title>Welcome to Slotify!</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	
+</head>
+
+<body>
+	
+<div id="mainContainer">
+		<div id="topContainer">
+			<?php include("includes/navbarContainer.php"); ?>
+		
+			<div id="mainViewContainer">
+				<div id="mainContent">
